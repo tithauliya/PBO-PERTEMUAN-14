4e04a0bee687f47be8437f5c6b41b0f7e5d1a678
tithauliya <tithauliya21@gmail.com>
2025-11-22 17:58:42 +0700

    Upload

---
+++ PERTEMUAN14/build/generated-sources/ap-source-output/Pertemuan12/DataRoti_.java
@@ -0,0 +1,21 @@
+package Pertemuan12;
+
+import Pertemuan12.Pelanggan;
+import Pertemuan12.Transaksi;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-11-22T17:47:08", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(DataRoti.class)
+public class DataRoti_ { 
+
+    public static volatile SingularAttribute<DataRoti, String> namaRoti;
+    public static volatile CollectionAttribute<DataRoti, Transaksi> transaksiCollection;
+    public static volatile SingularAttribute<DataRoti, Integer> harga;
+    public static volatile SingularAttribute<DataRoti, String> idRoti;
+    public static volatile SingularAttribute<DataRoti, Pelanggan> idPelanggan;
+    public static volatile SingularAttribute<DataRoti, Integer> stok;
+
+}
---
+++ PERTEMUAN14/build/generated-sources/ap-source-output/Pertemuan12/Pelanggan_.java
@@ -0,0 +1,21 @@
+package Pertemuan12;
+
+import Pertemuan12.DataRoti;
+import Pertemuan12.Transaksi;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-11-22T17:47:08", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Pelanggan.class)
+public class Pelanggan_ { 
+
+    public static volatile CollectionAttribute<Pelanggan, Transaksi> transaksiCollection;
+    public static volatile SingularAttribute<Pelanggan, String> noTelepon;
+    public static volatile SingularAttribute<Pelanggan, String> namaPelanggan;
+    public static volatile CollectionAttribute<Pelanggan, DataRoti> dataRotiCollection;
+    public static volatile SingularAttribute<Pelanggan, String> idPelanggan;
+    public static volatile SingularAttribute<Pelanggan, String> alamat;
+
+}
